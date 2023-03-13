459791ba41bfc6b1b061151b2042081481be7363
Juanjo <juanjo.cabremora@gmail.com>
2023-03-13 10:52:15 +0100

    some changes

--- oxfordreader/PointnetVLAD_submaps.py
+++ oxfordreader/PointnetVLAD_submaps.py
@@ -16,7 +16,7 @@ class PointnetVLAD_submap_reader():
         fig, ax = plt.subplots()
 
         # map poses
-        ax.scatter(train_easting, train_northing, c='grey', s=10)
+        ax.scatter(train_easting, train_northing, c='blue', s=10)
         ax.scatter(test_easting, test_northing, c='red', s=10)
 
         ax.axis('square')
@@ -44,8 +44,24 @@ class PointnetVLAD_submap_reader():
 
 if __name__ == "__main__":
     # Prepare data
-    oxford_read = PointnetVLAD_submap_reader(directory='/home/arvc/Escritorio/develop/SparseConv/benchmark_datasets/oxford/2014-11-14-16-34-33')
-    oxford_read2 = PointnetVLAD_submap_reader(directory='/home/arvc/Escritorio/develop/SparseConv/benchmark_datasets/oxford/2015-11-12-11-22-05')
+    # oxford_read1 = PointnetVLAD_submap_reader(directory='/home/arvc/Escritorio/develop/SparseConv/benchmark_datasets/oxford/2014-11-14-16-34-33')
+    # # oxford_read2 = PointnetVLAD_submap_reader(directory='/home/arvc/Escritorio/develop/SparseConv/benchmark_datasets/oxford/2015-11-12-11-22-05')
+    # oxford_read3 = PointnetVLAD_submap_reader(directory='/home/arvc/Escritorio/develop/SparseConv/benchmark_datasets/oxford/2014-12-09-13-21-02')
+    # oxford_read4 = PointnetVLAD_submap_reader(directory='/home/arvc/Escritorio/develop/SparseConv/benchmark_datasets/oxford/2015-02-03-08-45-10')
+    # oxford_read5 = PointnetVLAD_submap_reader(directory='/home/arvc/Escritorio/develop/SparseConv/benchmark_datasets/oxford/2015-04-24-08-15-07')
+    #
+    # oxford_read1.plot_submap()
+    # # oxford_read2.plot_submap()
+    # oxford_read3.plot_submap()
+    # oxford_read4.plot_submap()
+    # oxford_read5.plot_submap()
 
-    oxford_read.plot_submap()
-    oxford_read2.plot_submap()
+    import os
+
+    path = '/home/arvc/Escritorio/develop/SparseConv/benchmark_datasets/oxford/'
+    all_folders = sorted(os.listdir(path))
+    index_list = [5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 22, 24, 31, 32, 33, 38, 43, 44]
+    for index in index_list:
+        oxford_read = PointnetVLAD_submap_reader(directory=os.path.join(path,all_folders[index]))
+        oxford_read.plot_submap()
+
---
+++ some_combinations.py
@@ -0,0 +1,56 @@
+"""
+In this script we assess the labelling of some combinations of a given trajectory
+"""
+
+
+from run_3D_overlap import reader_manager
+import numpy as np
+import itertools as it
+import csv
+from config import EXP_PARAMETERS
+
+def process_overlap(keyframe_manager, poses, scan_idx, i):
+    pre_process = True
+
+    current_pose = poses[scan_idx].array
+    reference_pose = poses[i].array
+
+    if pre_process:
+        keyframe_manager.keyframes[scan_idx].pre_process()
+        keyframe_manager.keyframes[i].pre_process()
+
+    transformation_matrix = np.linalg.inv(current_pose).dot(reference_pose)
+    atb, rmse = keyframe_manager.compute_transformation_local_registration(scan_idx, i, method='point2point',
+                                                                           initial_transform=transformation_matrix)
+    overlap_pose = keyframe_manager.compute_3d_overlap(scan_idx, i, atb)
+
+    atb, rmse = keyframe_manager.compute_transformation_global_registration(scan_idx, i, method='FPFH')
+    overlap_fpfh = keyframe_manager.compute_3d_overlap(scan_idx, i, atb)
+
+    overlap = np.maximum(overlap_pose, overlap_fpfh)
+    return overlap, overlap_pose, overlap_fpfh
+
+
+if __name__ == "__main__":
+    scan_times, poses, pos, keyframe_manager, lat, lon = reader_manager(directory=EXP_PARAMETERS.directory)
+    scan_indices = np.arange(0, len(scan_times))
+    # scan_combinations = list(it.combinations(scan_indices, 2))
+    samples = np.random.uniform(-30, 10, (20, 2))
+
+    print(samples)
+
+
+    # with open(EXP_PARAMETERS.directory + '/labelling_prueba.csv', 'w', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(["Reference timestamp", "Other timestamp", "Overlap", "Overlap poses", "Overlap fpfh", "Reference x", "Reference y", "Other x", "Other y"])
+    #     for idx in scan_indices:
+    #         writer.writerow([scan_times[idx], scan_times[idx], 1.0, 1.0, 1.0, pos[idx, 0], pos[idx, 1], pos[idx, 0], pos[idx, 1]])
+    #     for i in range(0, len(scan_combinations)):
+    #         print('Calculated: ', i, 'overlaps out of ', len(scan_combinations))
+    #         idx_reference = scan_combinations[i][0]
+    #         idx_other = scan_combinations[i][1]
+    #         overlap, overlap_pose, overlap_fpfh = process_overlap(keyframe_manager, poses, idx_reference, idx_other)
+    #         writer.writerow([scan_times[idx_reference], scan_times[idx_other], overlap, overlap_pose, overlap_fpfh, pos[idx_reference, 0], pos[idx_reference, 1], pos[idx_other, 0], pos[idx_other, 1]])
+
+
+
