8215c5999fa1e062094b19a413e4806679512e73
juanjo-cabrera <juanjo.cabremora@gmail.com>
2023-06-22 14:50:47 +0200

    a testar

--- ml_tools/minkunet.py
+++ ml_tools/minkunet.py
@@ -29,8 +29,111 @@ import MinkowskiEngine as ME
 
 from MinkowskiEngine.modules.resnet_block import BasicBlock, Bottleneck
 
-from resnet import ResNetBase
+# from resnet import ResNetBase
+class ResNetBase(nn.Module):
+    BLOCK = None
+    LAYERS = ()
+    INIT_DIM = 64
+    PLANES = (64, 128, 256, 512)
 
+    def __init__(self, in_channels, out_channels, D=3):
+        nn.Module.__init__(self)
+        self.D = D
+        assert self.BLOCK is not None
+
+        self.network_initialization(in_channels, out_channels, D)
+        self.weight_initialization()
+
+    def network_initialization(self, in_channels, out_channels, D):
+
+        self.inplanes = self.INIT_DIM
+        self.conv1 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                in_channels, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiInstanceNorm(self.inplanes),
+            ME.MinkowskiReLU(inplace=True),
+            ME.MinkowskiMaxPooling(kernel_size=2, stride=2, dimension=D),
+        )
+
+        self.layer1 = self._make_layer(
+            self.BLOCK, self.PLANES[0], self.LAYERS[0], stride=2
+        )
+        self.layer2 = self._make_layer(
+            self.BLOCK, self.PLANES[1], self.LAYERS[1], stride=2
+        )
+        self.layer3 = self._make_layer(
+            self.BLOCK, self.PLANES[2], self.LAYERS[2], stride=2
+        )
+        self.layer4 = self._make_layer(
+            self.BLOCK, self.PLANES[3], self.LAYERS[3], stride=2
+        )
+
+        self.conv5 = nn.Sequential(
+            ME.MinkowskiDropout(),
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=3, dimension=D
+            ),
+            ME.MinkowskiInstanceNorm(self.inplanes),
+            ME.MinkowskiGELU(),
+        )
+
+        self.glob_pool = ME.MinkowskiGlobalMaxPooling()
+
+        self.final = ME.MinkowskiLinear(self.inplanes, out_channels, bias=True)
+
+    def weight_initialization(self):
+        for m in self.modules():
+            if isinstance(m, ME.MinkowskiConvolution):
+                ME.utils.kaiming_normal_(m.kernel, mode="fan_out", nonlinearity="relu")
+
+            if isinstance(m, ME.MinkowskiBatchNorm):
+                nn.init.constant_(m.bn.weight, 1)
+                nn.init.constant_(m.bn.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1, bn_momentum=0.1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                ME.MinkowskiConvolution(
+                    self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    dimension=self.D,
+                ),
+                ME.MinkowskiBatchNorm(planes * block.expansion),
+            )
+        layers = []
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride=stride,
+                dilation=dilation,
+                downsample=downsample,
+                dimension=self.D,
+            )
+        )
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes, planes, stride=1, dilation=dilation, dimension=self.D
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x: ME.SparseTensor):
+        x = self.conv1(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.conv5(x)
+        x = self.glob_pool(x)
+        return self.final(x)
 
 class MinkUNetBase(ResNetBase):
     BLOCK = None
@@ -246,37 +349,37 @@ class MinkUNet34C(MinkUNet34):
 
 
 if __name__ == '__main__':
-    from tests.python.common import data_loader
+    # from tests.python.common import data_loader
     # loss and network
-    criterion = nn.CrossEntropyLoss()
-    net = MinkUNet14A(in_channels=3, out_channels=5, D=2)
+    # criterion = nn.CrossEntropyLoss()
+    net = MinkUNet34C(in_channels=3, out_channels=32, D=3)
     print(net)
 
-    # a data loader must return a tuple of coords, features, and labels.
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    net = net.to(device)
-    optimizer = SGD(net.parameters(), lr=1e-2)
-
-    for i in range(10):
-        optimizer.zero_grad()
-
-        # Get new data
-        coords, feat, label = data_loader(is_classification=False)
-        input = ME.SparseTensor(feat, coordinates=coords, device=device)
-        label = label.to(device)
-
-        # Forward
-        output = net(input)
-
-        # Loss
-        loss = criterion(output.F, label)
-        print('Iteration: ', i, ', Loss: ', loss.item())
-
-        # Gradient
-        loss.backward()
-        optimizer.step()
-
-    # Saving and loading a network
-    torch.save(net.state_dict(), 'test.pth')
-    net.load_state_dict(torch.load('test.pth'))
+    # # a data loader must return a tuple of coords, features, and labels.
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #
+    # net = net.to(device)
+    # optimizer = SGD(net.parameters(), lr=1e-2)
+    #
+    # for i in range(10):
+    #     optimizer.zero_grad()
+    #
+    #     # Get new data
+    #     coords, feat, label = data_loader(is_classification=False)
+    #     input = ME.SparseTensor(feat, coordinates=coords, device=device)
+    #     label = label.to(device)
+    #
+    #     # Forward
+    #     output = net(input)
+    #
+    #     # Loss
+    #     loss = criterion(output.F, label)
+    #     print('Iteration: ', i, ', Loss: ', loss.item())
+    #
+    #     # Gradient
+    #     loss.backward()
+    #     optimizer.step()
+    #
+    # # Saving and loading a network
+    # torch.save(net.state_dict(), 'test.pth')
+    # net.load_state_dict(torch.load('test.pth'))
